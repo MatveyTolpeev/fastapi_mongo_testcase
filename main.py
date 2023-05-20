14106a4ccd1463da4844127bfd65565a5c21a8e0
MatveyTolpeev <matvey.tolpeev@mail.ru>
2023-05-21 01:32:07 +0500

    delete comments, start filters

--- main.py
+++ main.py
@@ -27,45 +27,10 @@ def get_database():
     return client['db']
 
 
-# async def load_json(file: UploadFile = File(...)):
-#     contents = await file.read()
-#     products_list = [Product(
-#         title=products_dict["title"],
-#         sku=products_dict["sku"],
-#         color=str(products_dict["color"]).split("/")[1],
-#         color_code=str(products_dict["color"]).split("/")[0],
-#         brand=Brand(name=products_dict["brand"],
-#                     slug=slugify(products_dict["brand"])),
-#         sex=products_dict["sku"],
-#         material=products_dict["sku"],
-#         root_category=Category(
-#             name=products_dict["root_category"],
-#             slug=products_dict["root_category"]),
-#         price=products_dict["sku"],
-#         discount_price=products_dict["sku"],
-#         in_the_sale=products_dict["sku"],
-#         leftovers=products_dict["sku"])
-#                      if products_dict["root_category"] != "Косметика"
-#                      else
-#                      Product(
-#                          title=products_dict["title"],
-#                          sku=products_dict["sku"],
-#                          color="",
-#                          brand=Brand(name=products_dict["brand"],
-#                                      slug=slugify(products_dict["brand"])),
-#                          sex=products_dict["sku"],
-#                          material=products_dict["sku"],
-#                          root_category=Category(
-#                              name=products_dict["root_category"],
-#                              slug=products_dict["root_category"]),
-#                          price=products_dict["sku"],
-#                          discount_price=products_dict["sku"],
-#                          in_the_sale=products_dict["sku"],
-#                          leftovers=products_dict["sku"])
-#                      for products_dict in json.loads(contents)]
-#     # json_compatible_item_data = jsonable_encoder(Brand(name='Bal', slug='777'))
-#     print(request.json())
-#     return await request.json()
+def drop_collection(collection_name: str):
+    db = get_database()
+    collection = db[collection_name]
+    collection.drop()
 
 
 test_leftovers = [
@@ -105,75 +70,12 @@ test_leftovers2 = [
 ]
 
 
-# def test_load_to_db():
-#     product = Product(
-#         title="кепка",
-#         sku=str("87O338-1129204").split("-")[0],
-#         color=str("005/коричнево-синий").split("/")[1],
-#         color_code=str("005/коричнево-синий").split("/")[0],
-#         brand=Brand(name="Corneliani",
-#                     slug=slugify("Corneliani")),
-#         sex="Ж",
-#         material=" 69% шерсть, 29% шелк, 2% кашемир",
-#         root_category=Category(name="Одежда аксессуары",
-#                                slug=slugify("Одежда аксессуары")),
-#         price=22320,
-#         discount_price=22320,
-#         in_the_sale=False,
-#         leftovers=[Leftover(size=lf["size"],
-#                             count=lf["count"],
-#                             price=lf["price"])
-#                    for lf in test_leftovers]
-#     )
-#     product2 = Product(
-#         title="кепка2",
-#         sku=str("87O3382-1129204").split("-")[0],
-#         color=str("005/коричнево-синий").split("/")[1],
-#         color_code=str("005/коричнево-синий").split("/")[0],
-#         brand=Brand(name="Corneliani",
-#                     slug=slugify("Corneliani")),
-#         sex="М",
-#         material=" 69% шерсть, 29% шелк, 2% кашемир",
-#         root_category=Category(name="Одежда аксессуары",
-#                                slug=slugify("Одежда аксессуары")),
-#         price=22320,
-#         discount_price=22320,
-#         in_the_sale=False,
-#         leftovers=[Leftover(size=lf["size"],
-#                             count=lf["count"],
-#                             price=lf["price"])
-#                    for lf in test_leftovers]
-#     )
-#     product3 = Product(
-#         title="кепка2",
-#         sku=str("87O338-11292404").split("-")[0],
-#         color=str("005/коричнево-синий").split("/")[1],
-#         color_code=str("005/коричнево-синий").split("/")[0],
-#         brand=Brand(name="Corneliani",
-#                     slug=slugify("Corneliani")),
-#         sex="М",
-#         material=" 69% шерсть, 29% шелк, 2% кашемир",
-#         root_category=Category(name="Одежда аксессуары",
-#                                slug=slugify("Одежда аксессуары")),
-#         price=22320,
-#         discount_price=22320,
-#         in_the_sale=False,
-#         leftovers=[Leftover(size=lf["size"],
-#                             count=lf["count"],
-#                             price=lf["price"])
-#                    for lf in test_leftovers2]
-#     )
-#     # print(product)
-#     return [product, product2, product3]
-
-
-def test_get_from_db():
+def get_from_db():
     pass
 
 
-def test_accept_data():
+def accept_data():
     products_list = requests.get("http://localhost:8000/api/v1/data").json()
-    print(len(products_list))
     search_arr = ["Обувь", "Одежда", "Сумки"]
     products = [
         Product(
@@ -192,13 +94,14 @@ def test_accept_data():
             root_category=Category(
                 name=products_dict["root_category"],
                 slug=slugify(products_dict["root_category"])),
-            price=products_dict["price"],
-            discount_price=products_dict["discount_price"],
+            price=products_dict["discount_price"] if products_dict["discount_price"] > 0
+            else products_dict["price"],
+            discount_price=0 if products_dict["discount_price"] >= products_dict["price"]
+            else products_dict["discount_price"],
             in_the_sale=products_dict["in_the_sale"],
             leftovers=products_dict["leftovers"])
         for products_dict in products_list
     ]
-    # print(result[0]["sku"])
     return products
 
 
@@ -217,13 +120,13 @@ def test_work(request: Request):
 
 
 def save_to_db():
-    result = test_accept_data()
-    print(type(result[0]))
-    print(result[0])
+    drop_collection("products")
     db = get_database()
     collection = db["products"]
-    for el in test_accept_data():
-        print(f"сейчас мы на товаре: {el.title}")
+    count = 0
+    for el in accept_data():
+        if count % 1000 == 0:
+            print(count)
         if collection.find_one({"sku": el.sku}) is None:
             collection.insert_one(el.dict())
             continue
@@ -233,46 +136,27 @@ def save_to_db():
                 for cur_leftover in cur["leftovers"]:
                     if cur_leftover["size"] == el_leftover.size:
                         pass
-                        print(f"размер {el_leftover.size} уже есть у товара {cur['title']}")
                         cur_leftover["count"] += el_leftover.count
             else:
-                print(f"размер {el_leftover.size} ещё нет у товара {cur['title']}")
                 cur["leftovers"] = cur["leftovers"] + [el_leftover.dict()]
         collection.replace_one({"sku": cur["sku"]}, cur)
+        count += 1
+
+
+def get_filtered_data():
+    pass
 
 
 routes = [
     APIRoute(path="/ping", endpoint=test_work, methods=['GET']),
-    # APIRoute(path="/load_json", endpoint=load_json, methods=['POST']),
-    APIRoute(path="/test_load", endpoint=test_accept_data, methods=['POST']),
     APIRoute(path="/api/v1/data", endpoint=send_data, methods=['GET']),
-    APIRoute(path="/get_data", endpoint=test_accept_data, methods=['GET']),
-    APIRoute(path="/save_to_db", endpoint=save_to_db, methods=['GET'])
+    APIRoute(path="/save_to_db", endpoint=save_to_db, methods=['GET']),
+    APIRoute(path="filtered_data", endpoint=get_filtered_data, methods=['POST'])
 ]
 
 app = FastAPI()
 app.include_router(APIRouter(routes=routes))
 
 if __name__ == "__main__":
-    # db = get_database()
-    # collection = db["products"]
-
-    # for el in test_load_to_db():
-    #     print(f"сейчас мы на товаре: {el.title}")
-    #     if collection.find_one({"sku": el.sku}) is None:
-    #         collection.insert_one(el.dict())
-    #         continue
-    #     cur = collection.find_one({"sku": el.sku})
-    #     for el_leftover in el.leftovers:
-    #         if any(temp_cur_leftover["size"] == el_leftover.size for temp_cur_leftover in cur["leftovers"]):
-    #             for cur_leftover in cur["leftovers"]:
-    #                 if cur_leftover["size"] == el_leftover.size:
-    #                     pass
-    #                     print(f"размер {el_leftover.size} уже есть у товара {cur['title']}")
-    #                     cur_leftover["count"] += el_leftover.count
-    #         else:
-    #             print(f"размер {el_leftover.size} ещё нет у товара {cur['title']}")
-    #             cur["leftovers"] = cur["leftovers"] + [el_leftover.dict()]
-    #     collection.replace_one({"sku": cur["sku"]}, cur)
 
     uvicorn.run(app, host="localhost", port=8000)
